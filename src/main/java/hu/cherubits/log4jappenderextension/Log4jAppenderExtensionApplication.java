35e4d76e260686671cc25de2e4d31150b2f90a03
lordoftheflies <heglas11@gmail.com>
2017-05-31 16:38:54 +0200

    Init from Spring initializer.

---
+++ src/main/java/hu/cherubits/log4jappenderextension/Log4jAppenderExtensionApplication.java
@@ -0,0 +1,12 @@
+package hu.cherubits.log4jappenderextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jAppenderExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jAppenderExtensionApplication.class, args);
+	}
+}
